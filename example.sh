d62de144076127e059efeed4cac3cd06901ad4a8
Zijian Huang <99373586+nelsonhuangzijian@users.noreply.github.com>
2023-02-08 00:56:17 +0800

    code release

--- example.sh
+++ example.sh
@@ -4,29 +4,14 @@ CUDA_VISIBLE_DEVICES=0 python main.py --cuda --do_train --do_valid --do_test \
   -lr 0.0005 --max_steps 100001 --cpu_num 1 --geo line --valid_steps 20000 \
   -linem "(1600,2)" -ql T
 
-CUDA_VISIBLE_DEVICES=0 python main-entailment.py --cuda --do_train --do_valid --do_test \
-  --data_path data/FB15k-237-betae -n 128 -b 512 -d 200 -g 60 \
-  -lr 0.0005 --max_steps 100001 --cpu_num 1 --geo line --valid_steps 20000 \
-  -linem "(1600,2)" -ql T
-
 # NELL
 CUDA_VISIBLE_DEVICES=0 python main.py --cuda --do_train --do_valid --do_test \
   --data_path data/NELL-betae -n 128 -b 512 -d 200 -g 60 \
   -lr 0.0005 --max_steps 100001 --cpu_num 1 --geo line --valid_steps 20000 \
   -linem "(1600,2)" -ql T
 
-CUDA_VISIBLE_DEVICES=0 python main-entailment.py --cuda --do_train --do_valid --do_test \
-  --data_path data/NELL-betae -n 128 -b 512 -d 200 -g 60 \
-  -lr 0.0005 --max_steps 100001 --cpu_num 1 --geo line --valid_steps 20000 \
-  -linem "(1600,2)" -ql T
-
 # WN18RR
 CUDA_VISIBLE_DEVICES=0 python main.py --cuda --do_train --do_valid --do_test \
-  --data_path data/WN18RR-betae -n 128 -b 512 -d 200 -g 60 \
-  -lr 0.0005 --max_steps 100001 --cpu_num 1 --geo line --valid_steps 20000 \
-  -linem "(1600,2)" -ql T
-
-CUDA_VISIBLE_DEVICES=0 python main-entailment.py --cuda --do_train --do_valid --do_test \
   --data_path data/WN18RR-betae -n 128 -b 512 -d 200 -g 60 \
   -lr 0.0005 --max_steps 100001 --cpu_num 1 --geo line --valid_steps 20000 \
   -linem "(1600,2)" -ql T
